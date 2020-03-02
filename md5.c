60b419c5680c171e0ed99143c1257ee6b1d26bd1
SlavLuk <dneprdaddy@gmail.com>
2020-03-02 23:58:43 +0000

    Added basic MD5 functions

---
+++ md5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+// Macros functions
+//Section 3.4 Step 4 process message in 16- word blocks
+ #define F(x,y,z)(((x) & (y)) | ((~x) & (z)))
+ #define G(x,y,z)(((x) & (z)) | ((y) & (~z)))
+ #define H(x,y,z,)((x) ^ (y) ^ (z))
+ #define I(x,y,z)((y) ^ ((x) | (~z)))
+
+
+
+	int main(int argc,char *argv[]){
+	
+	
+	printf("Hello Worlds");
+	
+	
+	
+	
+	
+	}
