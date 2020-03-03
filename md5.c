9532b54a0216e8a204fdb5e9bfdaefa1ec53fd86
SlavLuk <dneprdaddy@gmail.com>
2020-03-03 01:46:18 +0000

    Fixed comma

--- md5.c
+++ md5.c
@@ -6,7 +6,7 @@
 //Section 3.4 Step 4 process message in 16- word blocks
  #define F(x,y,z)(((x) & (y)) | ((~x) & (z)))
  #define G(x,y,z)(((x) & (z)) | ((y) & (~z)))
- #define H(x,y,z,)((x) ^ (y) ^ (z))
+ #define H(x,y,z)((x) ^ (y) ^ (z))
  #define I(x,y,z)((y) ^ ((x) | (~z)))
 
 // ROTATE_LEFT rotates x left n bits
